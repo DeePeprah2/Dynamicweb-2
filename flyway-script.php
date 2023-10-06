992b7c7494bd0f1dfaffe1716814b3935233983c
Dee <deebio2@yahoo.co.uk>
2023-10-06 07:41:03 +0100

    update

--- flyway-script.php
+++ flyway-script.php
@@ -1,7 +1,11 @@
 
+# bin! Bash
 # flyway script
 # command to download flyway
 wget -q- https://download.red-gate.com/maven/release/org/flywaydb/enterprise/flyway-commandline/9.21.1/flyway-commandline-9.21.1-linux-x64.tar.gz | tar -xvz && sudo ln -s `pwd`/flyway-9.21.1/flyway /usr/local/bin 
+
+# create a symbolic line to make flyway accessible globaly
+sudo ln -s $|(pwd)/flywayflyway-9.21.1 /usr/local/bin
 
 # change directory to flyway directory 
 cd flyway-9.21.1
@@ -10,10 +14,11 @@ cd flyway-9.21.1
 rm -rf sql
 
 # make a new sql directory
-mkdir sql
+sudo mkdir sql
 
 # 4b. command to copy file from aws , cd into flyway folder to use the command below
 aws s3 cp s3://dee-data-migration-bucket/V1__nest.sql /home/ec2-user/flyway-9.22.2/sql
+
 # run flyway migrate command
 flyway -url=jdbc:mysql://dbdatabase-1.c3qgmy74n7ea.eu-west-2.rds.amazonaws.com:3306/deebio \
 -user=dee\
